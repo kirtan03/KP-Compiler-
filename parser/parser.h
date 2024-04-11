0c271dac62e6412de4baa9f4f3e218ff66a21639
kirtan03 <kirtanpatel21092003@gmail.com>
2024-04-11 19:09:29 +0530

    Complete lexer and parser

---
+++ parser/parser.h
@@ -0,0 +1,24 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include <map>
+#include "lexer/lexer.h"
+#include "lexer/token.h"
+
+class ExprAST;
+class PrototypeAST;
+class FunctionAST;
+
+extern std::map<char, int> BinopPrecedence;
+std::unique_ptr<ExprAST> ParseNumberExpr();
+std::unique_ptr<ExprAST> ParseParenExpr();
+std::unique_ptr<ExprAST> ParseIdentifierExpr();
+std::unique_ptr<ExprAST> ParsePrimary();
+std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec, std::unique_ptr<ExprAST> LHS);
+std::unique_ptr<ExprAST> ParseExpression();
+std::unique_ptr<PrototypeAST> ParsePrototype();
+std::unique_ptr<FunctionAST> ParseDefinition();
+std::unique_ptr<FunctionAST> ParseTopLevelExpr();
+std::unique_ptr<PrototypeAST> ParseExtern();
+
+#endif
